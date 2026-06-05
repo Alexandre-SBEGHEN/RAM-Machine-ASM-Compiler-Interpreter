7719beed4a47cf7b2e3d88e6e93a6335af255c10
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-05 18:20:20 +0200

    tests(interpreter): Dernier programme test pour program_interpret

--- tests/test_interpreter.c
+++ tests/test_interpreter.c
@@ -36,7 +36,7 @@ void test_program_interpret() {
     Register* reg = register_create();
     Program* prog = program_create(16);
 
-    /* Programme test 1
+    /* Programme test 1 (a = 0)
         main:
             LOAD #1;
             STORE @0;
@@ -50,7 +50,7 @@ void test_program_interpret() {
     program_interpret(prog, mem, reg);
     assert(mem->data[0] == 1, "Erreur du programme test 1");
 
-    /* Programme test 2
+    /* Programme test 2 (a += 10)
         main:
             LOAD #-9;		# ]
             STORE @2;		# ] i = -9
@@ -93,6 +93,72 @@ void test_program_interpret() {
     mem->data[0] = -5;
     program_interpret(prog, mem, reg);
     assert(mem->data[0] == 5, "Erreur du programme test 2");
+    
+    /* Programme test 3 (valeur absolue)
+        main:
+            LOAD @0;	# ]
+            INCR;		# ] i = a+1
+            STORE @1;	# ]
+            
+            LOAD @0;	# ]
+            INCR;		# ] r = a+1
+
+            JZ absolute;	# si (a+1 <= 0) <=> (a <= -1) goto absolute
+            JUMP end;	# sinon pas besoin et fin
+
+        absolute:
+            LOAD @0;	# ]
+            INCR;		# ]
+            INCR;		# ] a += 2
+            STORE @0;	# ]
+
+            LOAD @1;	# ]
+            INCR;		# ] ++i
+            STORE @1;	# ]
+
+            JZ absolute;	# si i <= 0 goto recommencer
+
+        end:
+            HALT;
+    */
+    prog->instructions[0][0] = 2;
+    prog->instructions[0][1] = 0;
+    prog->instructions[1][0] = 4;
+    prog->instructions[2][0] = 3;
+    prog->instructions[2][1] = 1;
+    prog->instructions[3][0] = 2;
+    prog->instructions[3][1] = 0;
+    prog->instructions[4][0] = 4;
+    prog->instructions[5][0] = 7;
+    prog->instructions[5][1] = 7; //goto absolute
+    prog->instructions[6][0] = 6;
+    prog->instructions[6][1] = 15; //goto end
+    prog->instructions[7][0] = 2;
+    prog->instructions[7][1] = 0;
+    prog->instructions[8][0] = 4;
+    prog->instructions[9][0] = 4;
+    prog->instructions[10][0] = 3;
+    prog->instructions[10][1] = 0;
+    prog->instructions[11][0] = 2;
+    prog->instructions[11][1] = 1;
+    prog->instructions[12][0] = 4;
+    prog->instructions[13][0] = 3;
+    prog->instructions[13][1] = 1;
+    prog->instructions[14][0] = 7;
+    prog->instructions[14][1] = 7;
+    prog->instructions[15][0] = 8;
+    mem->data[0] = 0;
+    program_interpret(prog, mem, reg);
+    assert(mem->data[0] == 0, "Erreur du programme test 3");
+    mem->data[0] = 5;
+    program_interpret(prog, mem, reg);
+    assert(mem->data[0] == 5, "Erreur du programme test 3");
+    mem->data[0] = -1;
+    program_interpret(prog, mem, reg);
+    assert(mem->data[0] == 1, "Erreur du programme test 3");
+    mem->data[0] = -67;
+    program_interpret(prog, mem, reg);
+    assert(mem->data[0] == 67, "Erreur du programme test 3");
 
     program_delete(&prog);
     register_delete(&reg);
