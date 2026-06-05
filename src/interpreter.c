f29c8a84086d4a1944688366db903a56a2729286
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-05 10:23:24 +0200

    feat(interpreter): Corps de la fonction program_interpret
    Fonctionne avec un index qui parcoure la matrice du programme
    et exécute certaines actions en fonction de l'instruction et de
    l'opérande actuelle. Si l'interpréteur détecte une instruction
    inconnue, la boucle s'arrête. De même pour les sauts aux
    étiquettes, si l'indice en argument est négatif, la boucle s'arrête.
    
    Le code est flexible car il permet d'implémenter de nouvelles
    instructions très facilement.

--- src/interpreter.c
+++ src/interpreter.c
@@ -38,6 +38,54 @@ Program* program_create(const size_t number_of_instructions) {
     return prog;
 }
 
+// Exécution du programme compilé
+void program_interpret(const Program* prog, Memory* mem, Register* reg) {
+    size_t instruction_index = 0;
+
+    // Boucle du programme
+    while (1) {
+        long inst = prog[instruction_index][0]; // Instruction actuelle
+        long arg = prog[instruction_index][1];  // Argument / opérande actuel
+
+        // Action à faire selon l'instruction
+        switch (inst) {
+            // Ne rien faire
+            case 0: break;
+            // LOAD #?
+            case 1: load_direct(reg, arg); break;
+            // LOAD @?
+            case 2: load_from(reg, mem, arg); break;
+            // STORE @?
+            case 3: store_to(reg, mem, arg); break;
+            // INCR
+            case 4: increment(reg); break;
+            // DECR
+            case 5: decrement(reg); break;
+            // JUMP ?
+            case 6:
+                if (arg < 0) return;
+                instruction_index = arg;
+                continue;
+            // JZ ?
+            case 7:
+                if (arg < 0) return;
+                if (reg->val <= 0) {
+                    instruction_index = arg;
+                    continue;
+                }
+                break;
+            // HALT
+            case 8: return;
+            // Instruction inconnue (arrêt de l'interpréteur)
+            default:
+                return;
+        }
+
+        // Instruction suivante
+        ++instruction_index;
+    }
+}
+
 // Libération de la mémoire allouée pour Program et ses champs 
 void program_delete(Program** prog) {
     if (prog == NULL || *prog == NULL) return;
