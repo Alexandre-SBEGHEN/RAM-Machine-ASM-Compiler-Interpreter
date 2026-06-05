b3c2ae6c6926afc480e71c5c5529741ae2fcfc60
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-05 12:56:58 +0200

    tests(interpreter): tests pour program_create et program_interpret
    Ne fonctionnent pas encore car je me suis rendu compte d'une erreur
    dans la fonction program_interpret.

--- tests/test_interpreter.c
+++ tests/test_interpreter.c
@@ -10,8 +10,80 @@
 #include "machine.h"
 #include "interpreter.h"
 
-int main() {
+/**
+ * @brief Vérifie les choses suivantes :
+ * 
+ * - La création de la structure du programme
+ * 
+ * - Ses champs pointent vers des données existantes
+ */
+void test_program_create() {
+    Program* prog = program_create(1000);
+
+    // Test 1 : vérifier la création du prorgamme
+    assert(prog != NULL, "Echec de création du programme");
+
+    // Test 2 : vérifier que ses champs sont non nuls
+    assert(prog->instructions != NULL, "Echec de création du champ instructions");
+    assert(prog->data != NULL, "Echec de création du champ data");
+
+    program_delete(&prog);
+}
+
+
+void test_program_interpret() {
+    Memory* mem = memory_create();
+    Register* reg = register_create();
+    Program* prog = program_create(16);
+
+    /* Programme test 1
+        main:
+            LOAD #1;
+            STORE @0;
+            HALT;
+    */
+    prog->instructions[0][0] = 1;
+    prog->instructions[0][1] = 1;
+    prog->instructions[1][0] = 3;
+    prog->instructions[1][1] = 3;
+    prog->instructions[2][0] = 8;
+    printf("%ld\n", mem->data[0]);
+    program_interpret(prog, mem, reg);
+    printf("%ld\n", mem->data[0]);
+    assert(mem->data[0] == 1, "Erreur du programme test 1");
+
+    /* Programme test 2
+        main:
+            LOAD #-9;		# ]
+            STORE @2;		# ] i = -9
+        for:
+            LOAD @0;		# ]
+            INCR;			# ] ++a
+            STORE @0;		# ]
+
+            LOAD @2;		# ]
+            INCR;			# ] ++i
+            STORE @2;		# ]
+
+            JZ for;			# si i <= 0 goto for
+            
+            HALT;
     
+    prog->instructions[0][0] = 1;
+    prog->instructions[0][1] = 1;
+    prog->instructions[1][0] = 3;
+    prog->instructions[1][1] = 3;
+*/
+
+
+    program_delete(&prog);
+    register_delete(&reg);
+    memory_delete(&mem);
+}
+
+int main() {
+    test_program_create();
+    test_program_interpret();
 
     printf(COL_GREEN __FILE__ " - Tous les tests pour 'interpreter.c' sont au vert." COL_RESET);
     return 0;
