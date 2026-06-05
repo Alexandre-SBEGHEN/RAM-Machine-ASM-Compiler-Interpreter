5142a42faa3463b33af0c3b0da58a0174bb3d4f5
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-05 10:51:01 +0200

    feat(test): Ajout du fichier de tests pour l'interpréteur

---
+++ tests/test_interpreter.c
@@ -0,0 +1,18 @@
+/**
+ * @file test_interpreter.c
+ * @brief Fichier de tests des fonctions de interpreter.c.
+ * 
+ * @author Alexandre SBEGHEN
+ * @date 2026-06-à5
+ */
+#include <stdio.h>
+#include "test.h"
+#include "machine.h"
+#include "interpreter.h"
+
+int main() {
+    
+
+    printf(COL_GREEN __FILE__ " - Tous les tests pour 'interpreter.c' sont au vert." COL_RESET);
+    return 0;
+}
